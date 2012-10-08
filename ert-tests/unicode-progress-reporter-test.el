d07d5406a9afd9f7e6eda758941e390d3bbafa2b
Roland Walker <walker@pobox.com>
2012-10-08 09:41:13 -0400

    interactive tests with query

--- ert-tests/unicode-progress-reporter-test.el
+++ ert-tests/unicode-progress-reporter-test.el
@@ -22,120 +22,174 @@
 
 (ert-deftest unicode-progress-reporter-01 nil
   :tags '(:interactive)
-  (let ((ucs-utils-trade-memory-for-speed nil)
-        (ucs-utils-use-persistent-storage nil)
-        (unicode-progress-reporter-type "Horizontal Blocks"))
-    (unless (ucs-utils-char
-             (cadr (assoc unicode-progress-reporter-type unicode-progress-reporter-pulse-characters))
-             nil 'cdp)
-      (error (concat unicode-progress-reporter-type " are not displayable")))
-    (unicode-progress-reporter-setup)
-    (unicode-progress-reporter-test))
-  (unicode-progress-reporter-setup))
+  (should
+   (progn
+     (let ((cursor-in-echo-area t)
+           (ucs-utils-trade-memory-for-speed nil)
+           (ucs-utils-use-persistent-storage nil)
+           (unicode-progress-reporter-type "Horizontal Blocks"))
+       (read-char (format "Press a key to generate a progress reporter using: %s." unicode-progress-reporter-type))
+       (unless (ucs-utils-char
+                (cadr (assoc unicode-progress-reporter-type unicode-progress-reporter-pulse-characters))
+                nil 'cdp)
+         (error (concat unicode-progress-reporter-type " are not displayable")))
+       (unicode-progress-reporter-setup)
+       (setq cursor-in-echo-area nil)
+       (unicode-progress-reporter-test))
+     (unicode-progress-reporter-setup)
+     (y-or-n-p "Did that work as expected?"))))
 
 (ert-deftest unicode-progress-reporter-02 nil
   :tags '(:interactive)
-  (let ((ucs-utils-trade-memory-for-speed nil)
-        (ucs-utils-use-persistent-storage nil)
-        (unicode-progress-reporter-type "Moons"))
-    (unless (ucs-utils-char
-             (cadr (assoc unicode-progress-reporter-type unicode-progress-reporter-pulse-characters))
-             nil 'cdp)
-      (error (concat unicode-progress-reporter-type " are not displayable")))
-    (unicode-progress-reporter-setup)
-    (unicode-progress-reporter-test))
-  (unicode-progress-reporter-setup))
+  (should
+   (progn
+     (let ((cursor-in-echo-area t)
+           (ucs-utils-trade-memory-for-speed nil)
+           (ucs-utils-use-persistent-storage nil)
+           (unicode-progress-reporter-type "Moons"))
+       (read-char (format "Press a key to generate a progress reporter using: %s." unicode-progress-reporter-type))
+       (unless (ucs-utils-char
+                (cadr (assoc unicode-progress-reporter-type unicode-progress-reporter-pulse-characters))
+                nil 'cdp)
+         (error (concat unicode-progress-reporter-type " are not displayable")))
+       (unicode-progress-reporter-setup)
+       (setq cursor-in-echo-area nil)
+       (unicode-progress-reporter-test))
+     (unicode-progress-reporter-setup)
+     (y-or-n-p "Did that work as expected?"))))
 
 (ert-deftest unicode-progress-reporter-03 nil
   :tags '(:interactive)
-  (let ((ucs-utils-trade-memory-for-speed nil)
-        (ucs-utils-use-persistent-storage nil)
-        (unicode-progress-reporter-type "Vertical Blocks"))
-    (unless (ucs-utils-char
-             (cadr (assoc unicode-progress-reporter-type unicode-progress-reporter-pulse-characters))
-             nil 'cdp)
-      (error (concat unicode-progress-reporter-type " are not displayable")))
-    (unicode-progress-reporter-setup)
-    (unicode-progress-reporter-test))
-  (unicode-progress-reporter-setup))
+  (should
+   (progn
+     (let ((cursor-in-echo-area t)
+           (ucs-utils-trade-memory-for-speed nil)
+           (ucs-utils-use-persistent-storage nil)
+           (unicode-progress-reporter-type "Vertical Blocks"))
+       (read-char (format "Press a key to generate a progress reporter using: %s." unicode-progress-reporter-type))
+       (unless (ucs-utils-char
+                (cadr (assoc unicode-progress-reporter-type unicode-progress-reporter-pulse-characters))
+                nil 'cdp)
+         (error (concat unicode-progress-reporter-type " are not displayable")))
+       (unicode-progress-reporter-setup)
+       (setq cursor-in-echo-area nil)
+       (unicode-progress-reporter-test))
+     (unicode-progress-reporter-setup)
+     (y-or-n-p "Did that work as expected?"))))
 
 (ert-deftest unicode-progress-reporter-04 nil
   :tags '(:interactive)
-  (let ((ucs-utils-trade-memory-for-speed nil)
-        (ucs-utils-use-persistent-storage nil)
-        (unicode-progress-reporter-type "Vertical Counting Rods"))
-    (unless (ucs-utils-char
-             (cadr (assoc unicode-progress-reporter-type unicode-progress-reporter-pulse-characters))
-             nil 'cdp)
-      (error (concat unicode-progress-reporter-type " are not displayable")))
-    (unicode-progress-reporter-setup)
-    (unicode-progress-reporter-test))
-  (unicode-progress-reporter-setup))
+  (should
+   (progn
+     (let ((cursor-in-echo-area t)
+           (ucs-utils-trade-memory-for-speed nil)
+           (ucs-utils-use-persistent-storage nil)
+           (unicode-progress-reporter-type "Vertical Counting Rods"))
+       (read-char (format "Press a key to generate a progress reporter using: %s." unicode-progress-reporter-type))
+       (unless (ucs-utils-char
+                (cadr (assoc unicode-progress-reporter-type unicode-progress-reporter-pulse-characters))
+                nil 'cdp)
+         (error (concat unicode-progress-reporter-type " are not displayable")))
+       (unicode-progress-reporter-setup)
+       (setq cursor-in-echo-area nil)
+       (unicode-progress-reporter-test))
+     (unicode-progress-reporter-setup)
+     (y-or-n-p "Did that work as expected?"))))
 
 (ert-deftest unicode-progress-reporter-05 nil
   :tags '(:interactive)
-  (let ((ucs-utils-trade-memory-for-speed nil)
-        (ucs-utils-use-persistent-storage nil)
-        (unicode-progress-reporter-type "Clocks"))
-    (unless (ucs-utils-char
-             (cadr (assoc unicode-progress-reporter-type unicode-progress-reporter-pulse-characters))
-             nil 'cdp)
-      (error (concat unicode-progress-reporter-type " are not displayable")))
-    (unicode-progress-reporter-setup)
-    (unicode-progress-reporter-test))
-  (unicode-progress-reporter-setup))
+  (should
+   (progn
+     (let ((cursor-in-echo-area t)
+           (ucs-utils-trade-memory-for-speed nil)
+           (ucs-utils-use-persistent-storage nil)
+           (unicode-progress-reporter-type "Clocks"))
+       (read-char (format "Press a key to generate a progress reporter using: %s." unicode-progress-reporter-type))
+       (unless (ucs-utils-char
+                (cadr (assoc unicode-progress-reporter-type unicode-progress-reporter-pulse-characters))
+                nil 'cdp)
+         (error (concat unicode-progress-reporter-type " are not displayable")))
+       (unicode-progress-reporter-setup)
+       (setq cursor-in-echo-area nil)
+       (unicode-progress-reporter-test))
+     (unicode-progress-reporter-setup)
+     (y-or-n-p "Did that work as expected?"))))
 
 (ert-deftest unicode-progress-reporter-06 nil
   :tags '(:interactive)
-  (let ((ucs-utils-trade-memory-for-speed nil)
-        (ucs-utils-use-persistent-storage nil)
-        (unicode-progress-reporter-type "Ogham Letters"))
-    (unless (ucs-utils-char
-             (cadr (assoc unicode-progress-reporter-type unicode-progress-reporter-pulse-characters))
-             nil 'cdp)
-      (error (concat unicode-progress-reporter-type " are not displayable")))
-    (unicode-progress-reporter-setup)
-    (unicode-progress-reporter-test))
-  (unicode-progress-reporter-setup))
+  (should
+   (progn
+     (let ((cursor-in-echo-area t)
+           (ucs-utils-trade-memory-for-speed nil)
+           (ucs-utils-use-persistent-storage nil)
+           (unicode-progress-reporter-type "Ogham Letters"))
+       (read-char (format "Press a key to generate a progress reporter using: %s." unicode-progress-reporter-type))
+       (unless (ucs-utils-char
+                (cadr (assoc unicode-progress-reporter-type unicode-progress-reporter-pulse-characters))
+                nil 'cdp)
+         (error (concat unicode-progress-reporter-type " are not displayable")))
+       (unicode-progress-reporter-setup)
+       (setq cursor-in-echo-area nil)
+       (unicode-progress-reporter-test))
+     (unicode-progress-reporter-setup)
+     (y-or-n-p "Did that work as expected?"))))
 
 (ert-deftest unicode-progress-reporter-07 nil
   :tags '(:interactive)
-  (let ((ucs-utils-trade-memory-for-speed nil)
-        (ucs-utils-use-persistent-storage nil)
-        (unicode-progress-reporter-type "Horizontal Counting Rods"))
-    (unless (ucs-utils-char
-             (cadr (assoc unicode-progress-reporter-type unicode-progress-reporter-pulse-characters))
-             nil 'cdp)
-      (error (concat unicode-progress-reporter-type " are not displayable")))
-    (unicode-progress-reporter-setup)
-    (unicode-progress-reporter-test))
-  (unicode-progress-reporter-setup))
+  (should
+   (progn
+     (let ((cursor-in-echo-area t)
+           (ucs-utils-trade-memory-for-speed nil)
+           (ucs-utils-use-persistent-storage nil)
+           (unicode-progress-reporter-type "Horizontal Counting Rods"))
+       (read-char (format "Press a key to generate a progress reporter using: %s." unicode-progress-reporter-type))
+       (unless (ucs-utils-char
+                (cadr (assoc unicode-progress-reporter-type unicode-progress-reporter-pulse-characters))
+                nil 'cdp)
+         (error (concat unicode-progress-reporter-type " are not displayable")))
+       (unicode-progress-reporter-setup)
+       (setq cursor-in-echo-area nil)
+       (unicode-progress-reporter-test))
+     (unicode-progress-reporter-setup)
+     (y-or-n-p "Did that work as expected?"))))
 
 (ert-deftest unicode-progress-reporter-08 nil
   :tags '(:interactive)
-  (let ((ucs-utils-trade-memory-for-speed nil)
-        (ucs-utils-use-persistent-storage nil)
-        (unicode-progress-reporter-type "Triangles"))
-    (unless (ucs-utils-char
-             (cadr (assoc unicode-progress-reporter-type unicode-progress-reporter-pulse-characters))
-             nil 'cdp)
-      (error (concat unicode-progress-reporter-type " are not displayable")))
-    (unicode-progress-reporter-setup)
-    (unicode-progress-reporter-test))
-  (unicode-progress-reporter-setup))
+  (should
+   (progn
+     (let ((cursor-in-echo-area t)
+           (ucs-utils-trade-memory-for-speed nil)
+           (ucs-utils-use-persistent-storage nil)
+           (unicode-progress-reporter-type "Triangles"))
+       (read-char (format "Press a key to generate a progress reporter using: %s." unicode-progress-reporter-type))
+       (unless (ucs-utils-char
+                (cadr (assoc unicode-progress-reporter-type unicode-progress-reporter-pulse-characters))
+                nil 'cdp)
+         (error (concat unicode-progress-reporter-type " are not displayable")))
+       (unicode-progress-reporter-setup)
+       (setq cursor-in-echo-area nil)
+       (unicode-progress-reporter-test))
+     (unicode-progress-reporter-setup)
+     (y-or-n-p "Did that work as expected?"))))
 
 (ert-deftest unicode-progress-reporter-09 nil
   :tags '(:interactive)
-  (let ((ucs-utils-trade-memory-for-speed nil)
-        (ucs-utils-use-persistent-storage nil)
-        (unicode-progress-reporter-type "ASCII"))
-    (unless (ucs-utils-char
-             (cadr (assoc unicode-progress-reporter-type unicode-progress-reporter-pulse-characters))
-             nil 'cdp)
-      (error (concat unicode-progress-reporter-type " are not displayable")))
-    (unicode-progress-reporter-setup)
-    (unicode-progress-reporter-test))
-  (unicode-progress-reporter-setup))
+  (should
+   (progn
+     (let ((cursor-in-echo-area t)
+           (ucs-utils-trade-memory-for-speed nil)
+           (ucs-utils-use-persistent-storage nil)
+           (unicode-progress-reporter-type "ASCII"))
+       (read-char (format "Press a key to generate a progress reporter using: %s." unicode-progress-reporter-type))
+       (unless (ucs-utils-char
+                (cadr (assoc unicode-progress-reporter-type unicode-progress-reporter-pulse-characters))
+                nil 'cdp)
+         (error (concat unicode-progress-reporter-type " are not displayable")))
+       (unicode-progress-reporter-setup)
+       (setq cursor-in-echo-area nil)
+       (unicode-progress-reporter-test))
+     (unicode-progress-reporter-setup)
+     (y-or-n-p "Did that work as expected?"))))
 
 ;;
 ;; Emacs
